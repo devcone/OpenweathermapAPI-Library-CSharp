9d438155b6bbbada2aaec33cd1152a35eb4461aa
itsrx <itsrx@TOBYSPC>
2023-03-20 18:50:58 +0000

    Add project files.

---
+++ openWeatherAPILib/getXML.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace openWeatherAPILib
+{
+    class getXML
+    {
+        public static string buildRequest(string apiKey, string locationName)
+        {
+            string queryURL = "http://api.openweathermap.org/data/2.5/weather?q="
+            + locationName + "&mode=xml&units=metric&APPID=" + apiKey;
+
+            return queryURL;
+        }
+
+        public static XmlDocument requestXMLDoc(string queryURL)
+        {
+            using (WebClient client = new WebClient())
+            {
+                string xmlContent = client.DownloadString(queryURL);
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.LoadXml(xmlContent);
+                return xmlDocument;
+            }
+
+
+        }
+
+        public static List<string> getDataFromXML(XmlDocument weatherXML)
+        {
+
+        }
+    }
+}
---
+++ openWeatherAPILib/main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace openWeatherAPILib
+{
+    public class main
+    {
+        public static string startRequest(string apiKey, string locationName)
+        {
+            // Building the query URL and obtaining the SQL document from the API
+            string queryURL = getXML.buildRequest(apiKey, locationName);
+            XmlDocument weatherDataXML = getXML.requestXMLDoc(queryURL);
+
+            return queryURL;
+        }
+
+
+
+    }
+}
